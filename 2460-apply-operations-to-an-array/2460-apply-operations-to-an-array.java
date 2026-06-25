c8c63f68443b95daa3040f7da7d9a1d953b7c957
Samarth Kolse <samarthkolse@gmail.com>
2026-06-25 10:51:39 +0530

    Time: 1 ms (95.41%), Space: 45.5 MB (8.17%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -12,19 +12,12 @@ class Solution {
         int j = 0;
         while (j < nums.length) {
             if (nums[j] != 0) {
-
                 int temp = nums[i];
-
                 nums[i] = nums[j];
-
                 nums[j] = temp;
-
                 i++;
-
             }
-
             j++;
-
         }
         return nums;
     }
